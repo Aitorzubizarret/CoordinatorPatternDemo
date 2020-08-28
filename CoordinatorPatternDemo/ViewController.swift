54664fd313acf8da77630cc271f3b12167ab61c1
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2020-08-28 20:02:53 +0200

    Initial Commit

---
+++ CoordinatorPatternDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorPatternDemo
+//
+//  Created by Aitor Zubizarreta on 28/08/2020.
+//  Copyright © 2020 Aitor Zubizarreta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
